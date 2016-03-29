1ab1ba29b95680146000f90188697632f3f2a249
murych <murychkun@yahoo.com>
2016-03-29 22:52:34 +0300

    removed bubble sort, using built-in sort

--- vk_exercise.py
+++ vk_exercise.py
@@ -2,24 +2,16 @@ import vk
 
 user_profile_url = 'https://vk.com/may.timur'
 
-def bubblle(A):
-	for j in range(len(A) - 1):
-		for i in range(len(A) - 1):
-			if A[i]['likes']['count'] > A[i + 1]['likes']['count']:
-				A[i]['likes']['count'], A[i+1]['likes']['count'] = A[i+1]['likes']['count'], A[i]['likes']['count']
-	return A
-
 
 def get_most_liked_url(user_profile_url):
 	user_id = user_profile_url.split("/")[-1]
-	user = api.users.get(user_ids=user_id, fields='domain')
-	user_domain = user[0]['domain']
-	wall_posts = del api.wall.get(domain=user_domain)[0]
-	wall_posts = bubblle(wall_posts)
+	user = api.users.get(user_ids=user_id, fields='domain')[0]
+	user_domain = user['domain']
+	wall_posts = api.wall.get(domain=user_domain)[1:]
+	wall_posts.sort(key = lambda i: i['likes']['count'])
 	most_liked = wall_posts[-1]
 	most_liked_url = 'https://vk.com/wall%s_%s' % (most_liked['from_id'], most_liked['id'])
 	return most_liked_url
-
 
 if __name__ == '__main__':
 	try:
